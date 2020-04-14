60b33943d7266c60f9cb3a2f57d27b400bc1b935
garlicsuter <markasuter@gmail.com>
2020-04-14 19:58:15 -0400

    live demo!

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private float xRange = 6.0f;
+    private float yRange = 4.15f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //move Player
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+        print("the value of moveHorizontal: " + moveVertical);
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        Debug.Log("movement: " + movement);
+
+        transform.Translate(movement * speed * Time.deltaTime);
+
+        //Keep me in the xRange
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector2(xRange, transform.position.y);
+        }
+
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector2(-xRange, transform.position.y);
+        }
+
+        if (transform.position.y > yRange)
+        {
+            transform.position = new Vector2(transform.position.x,yRange );
+        }
+
+        if (transform.position.y < -yRange)
+        {
+            transform.position = new Vector2(transform.position.x, -yRange);
+        }
+    }
+}
